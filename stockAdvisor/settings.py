bd3b4495026bf68ac4e3295958bb5d24f96f8ff0
HnrqHolanda <henriqholanda1@gmail.com>
2025-02-11 08:41:16 -0300

    Update pw

--- stockAdvisor/settings.py
+++ stockAdvisor/settings.py
@@ -137,5 +137,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'stockadvisor1234@gmail.com'  
-EMAIL_HOST_PASSWORD = 'rgdk ddej jubx hgtg'  
+EMAIL_HOST_PASSWORD = 'bqgg ihar fwia slkq'  
 DEFAULT_FROM_EMAIL = 'stockadvisor1234@gmail.com'  
